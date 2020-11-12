b3a508b46406dc1e323246a4a35f0e5000d17c7c
mvb <mvaronabueno@gmail.com>
2020-11-12 18:20:48 +0100

    Added other type of background animation

--- components/js/main.js
+++ components/js/main.js
@@ -4,16 +4,27 @@ function printCurrentYear() {
 
 function updateBgc(){
 	var velocity = 0.3;
-	var pos = $(window).scrollTop(); 
-	$('section').each(function() { 
-		var $element = $(this);
-		var height = $element.height()-18;
-		$(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
-	});
+	// var pos = $(window).scrollTop(); 
+	// $('section').each(function() { 
+	// 	var $element = $(this);
+	// 	var height = $element.height()-18;
+	// 	$(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
+	// });
 }
 
 function setUpScrollEffect(){
 	$(window).bind('scroll', updateBgc);
+
+	var movementStrength = 25;
+	var height = movementStrength / $(window).height();
+	var width = movementStrength / $(window).width();
+	$("*").mousemove(function(e){
+		var pageX = e.pageX - ($(window).width() / 2);
+		var pageY = e.pageY - ($(window).height() / 2);
+		var newvalueX = width * pageX * -1 - 25;
+		var newvalueY = height * pageY * -1 - 50;
+		$('section').css("background-position", newvalueX+"px     "+newvalueY+"px");
+	});
 }
 
 function renderGeneralSettings(){
