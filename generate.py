9da0665dcbeda96ac7f6113394aafb5f9d825397
mvb <mvaronabueno@gmail.com>
2020-11-13 16:50:59 +0100

    Added populate_header

--- generate.py
+++ generate.py
@@ -24,16 +24,51 @@ def create_folders_for_lans(lans):
 
 def populate_independent_vars(file, json_file, lan):
 	for var in json_file:
+		regex_base_var = ''
 		if var.endswith('_' + lan):
 			base_var = var[:-(len(lan)+1)]
 			regex_base_var = '$' + base_var
-			with open(file, 'r+') as f:
-				file_content = f.read()
-				if regex_base_var in file_content:
-					new_file = file_content.replace(regex_base_var, json_file[var])
-					f.seek(0)
-					f.write(new_file)
-					f.close()
+		else:
+			regex_base_var = '$' + var
+
+		with open(file, 'r+') as f:
+			file_content = f.read()
+
+			if not isinstance(regex_base_var, list) and regex_base_var in file_content:
+				new_file = file_content.replace(str(regex_base_var), json_file[var])
+				new_file = new_file.replace('$lan', lan)
+				f.seek(0)
+				f.write(new_file)
+				f.close()
+
+def populate_header(file, json_file, active_lan):
+	(filename, ext) = os.path.splitext(file.split(os.path.sep)[-1])
+	with open(file, 'r+') as f:
+		file_content = f.read()
+
+		regex_lans = '{$list_lans}'
+
+		if regex_lans in file_content:
+			component_path = COMPONENTS_FOLDER_NAME + os.path.sep + COMPONENTS_HTML_FOLDER_NAME + os.path.sep + 'nav_lan.html'
+			component_new_content = ''
+			for lan_json in json_file['lans']:
+				with open(component_path, 'r+') as c:
+					component_content = c.read()
+					component_content = component_content.replace('$lan', lan_json)
+					component_content = component_content.replace('$filename', filename)
+
+					if lan_json == active_lan:
+						component_content = component_content.replace('$active', 'active')
+					else:
+						component_content = component_content.replace('$active', '')
+
+					component_new_content = component_new_content + component_content
+
+			new_file = file_content.replace(regex_lans, component_new_content)
+			f.seek(0)
+			f.write(new_file)
+			c.close()
+	f.close()
 				
 
 def create_index_for_lans(lans, json_file):
@@ -41,6 +76,7 @@ def create_index_for_lans(lans, json_file):
 		index_path = WEB_FOLDER_NAME + os.path.sep + lan + os.path.sep + 'index.html'
 		shutil.copy(COMPONENTS_FOLDER_NAME + os.path.sep + COMPONENTS_HTML_FOLDER_NAME + os.path.sep + 'index.html', index_path)
 		populate_independent_vars(index_path, json_file, lan)
+		populate_header(index_path, json_file, lan)
 		
 
 if __name__=="__main__":
